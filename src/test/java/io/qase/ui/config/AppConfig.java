8ab0a1968dd0466a98908bc36d315c15375c6cce
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-11-10 19:08:53 +0400

    remove merge policy in AppConfig

--- src/test/java/io/qase/ui/config/AppConfig.java
+++ src/test/java/io/qase/ui/config/AppConfig.java
@@ -2,7 +2,6 @@ package io.qase.ui.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:env",
         "classpath:config/${env}.properties"
